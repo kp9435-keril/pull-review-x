e426ca214ac7443a77a0da1057948a3f983cf78e
Keril Patel <keriljayeshbhai.patel@zebra.com>
2025-02-25 19:03:30 +0530

    wip: review pr function

--- src/review_pr.py
+++ src/review_pr.py
@@ -1,8 +1,23 @@
+from typing import Any
 from src.azure_openai_client import AzureOpenAIClient
 from src.github_client import GitHubClient
+from src.helpers import EnvironmentVariableHelper
 
 
 class ReviewPR:
     def __init__(self):
         self.github_client = GitHubClient()
         self.azure_openai_client = AzureOpenAIClient()
+    
+    def review_pr(self):
+        pr_diffs = self.git_manager.get_pr_diff_files()
+
+        get_pr_summary = EnvironmentVariableHelper.get_pr_summary()
+        if get_pr_summary:
+            self.get_pr_summary(pr_diffs=pr_diffs)
+
+        EnvironmentVariableHelper.get_pr_suggest_changes()
+    
+    def get_pr_summary(self, pr_diffs: dict[str, Any]) -> None:
+        print("PR Summary")
+        print(pr_diffs)
