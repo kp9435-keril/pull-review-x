e7823586ab8c16fb8dd96c05729d13c26c0ea475
Keril Patel <keriljayeshbhai.patel@zebra.com>
2025-02-25 17:41:08 +0530

    feat: added helpers

---
+++ src/helpers.py
@@ -0,0 +1,40 @@
+import os
+
+from src.constants import *
+
+
+class EnvironmentVariableHelper:
+    @staticmethod
+    def get_github_auth_token() -> str:
+        return EnvironmentVariableHelper.get_env_var(GITHUB_AUTH_TOKEN, None)
+    
+    @staticmethod
+    def get_repo() -> str:
+        return EnvironmentVariableHelper.get_env_var(REPO, None)
+    
+    @staticmethod
+    def get_azure_openai_apikey() -> str:
+        return EnvironmentVariableHelper.get_env_var(AZURE_OPENAI_APIKEY, None)
+
+    @staticmethod
+    def get_azure_openai_endpoint() -> str:
+        return EnvironmentVariableHelper.get_env_var(AZURE_OPENAI_ENDPOINT, None)
+    
+    @staticmethod
+    def get_azure_openai_model() -> str:
+        return EnvironmentVariableHelper.get_env_var(AZURE_OPENAI_MODEL, None)
+    
+    @staticmethod
+    def get_azure_openai_model_api_version() -> str:
+        return EnvironmentVariableHelper.get_env_var(AZURE_OPENAI_MODEL_API_VERSION, None)
+    
+    @staticmethod
+    def get_pr_summary() -> bool:
+        return EnvironmentVariableHelper.get_env_var(PR_SUMMARY, True)
+    
+    def get_pr_suggest_changes() -> bool:
+        return EnvironmentVariableHelper.get_env_var(PR_SUGGEST_CHANGES, True)
+
+    @staticmethod
+    def get_env_var(name: str, fallback: any) -> any:
+        return os.environ.get(name, fallback)
