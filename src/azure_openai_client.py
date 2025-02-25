b0844fe62d50c2e96361af6195629de9e8b3abbe
Keril Patel <keriljayeshbhai.patel@zebra.com>
2025-02-25 17:11:43 +0530

    feat: updated azure openai client

--- src/azure_openai_client.py
+++ src/azure_openai_client.py
@@ -6,10 +6,10 @@ from src.exceptions import InvalidOpenAIConfigException
 
 class AzureOpenAIClient:
     def __init__(self):
+        self.api_key = os.environ.get("AZURE_OPENAI_APIKEY", None)
+        self.endpoint = os.environ.get("AZURE_OPENAI_ENDPOINT", None)
         self.model = os.environ.get("AZURE_OPENAI_MODEL", None)
         self.model_api_version = os.environ.get("AZURE_OPENAI_MODEL_API_VERSION", None)
-        self.endpoint = os.environ.get("AZURE_OPENAI_ENDPOINT", None)
-        self.api_key = os.environ.get("AZURE_OPENAI_API_KEY", None)
         if not self.model or not self.model_api_version or not self.endpoint or not self.api_key:
             raise InvalidOpenAIConfigException("Azure OpenAI model, model API version, endpoint and API key are required")
         self.azure_openai_client = AzureOpenAI(
