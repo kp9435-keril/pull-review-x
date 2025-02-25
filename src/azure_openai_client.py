9a8e26c612e4da8e7955df8bbf68b561c54b22b8
Keril Patel <keriljayeshbhai.patel@zebra.com>
2025-02-25 17:41:27 +0530

    update: updated clients

--- src/azure_openai_client.py
+++ src/azure_openai_client.py
@@ -2,14 +2,15 @@ import os
 
 from openai import AzureOpenAI
 from src.exceptions import InvalidOpenAIConfigException
+from src.helpers import EnvironmentVariableHelper
 
 
 class AzureOpenAIClient:
     def __init__(self):
-        self.api_key = os.environ.get("AZURE_OPENAI_APIKEY", None)
-        self.endpoint = os.environ.get("AZURE_OPENAI_ENDPOINT", None)
-        self.model = os.environ.get("AZURE_OPENAI_MODEL", None)
-        self.model_api_version = os.environ.get("AZURE_OPENAI_MODEL_API_VERSION", None)
+        self.api_key = EnvironmentVariableHelper.get_azure_openai_apikey()
+        self.endpoint = EnvironmentVariableHelper.get_azure_openai_endpoint()
+        self.model = EnvironmentVariableHelper.get_azure_openai_model()
+        self.model_api_version = EnvironmentVariableHelper.get_azure_openai_model_api_version()
         if not self.model or not self.model_api_version or not self.endpoint or not self.api_key:
             raise InvalidOpenAIConfigException("Azure OpenAI model, model API version, endpoint and API key are required")
         self.azure_openai_client = AzureOpenAI(
--- src/github_client.py
+++ src/github_client.py
@@ -1,14 +1,15 @@
 import os
 
 from src.exceptions import *
+from src.helpers import EnvironmentVariableHelper
 
 
 class GitHubClient:
     def __init__(self):
-        self.token = os.environ.get("GITHUB_AUTH_TOKEN", None)
+        self.token = EnvironmentVariableHelper.get_github_auth_token()
         if not self.token:
             raise GitTokenMissingException("GitHub Token is required for REST APIs")
-        self.repositoryLink = os.environ.get("REPO", None)
+        self.repositoryLink = EnvironmentVariableHelper.get_repo()
         if not self.repositoryLink:
             raise RepoMissingException("Repository is required for REST APIs")
         self.owner, self.repo = self.repositoryLink.split("/")
