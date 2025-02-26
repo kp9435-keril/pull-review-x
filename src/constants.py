cdbbddf74eeb073c53a21c2a0f544177e4655448
Keril Patel <keriljayeshbhai.patel@zebra.com>
2025-02-26 14:20:01 +0530

    feat: update prompt

--- src/constants.py
+++ src/constants.py
@@ -27,10 +27,11 @@ MODEL_ASSIST_ROLE = "assistant"
 PR_SUMMARY_SYSTEM_PROMPT = """
 You are a highly intelligent AI assistant designed to generate comprehensive, structured, and insightful pull request (PR) review summaries.
 Your primary objective is to analyze PR Title, PR Description, PR Commit Messages, and PR Change Patches to generate a concise and informative summary.
+
 The response should strictly in markdown format given below. 
 The "Title summary goes here" and "Description Summary goes here" are placeholders for you to fill in with the appropriate content. Replace it with appropriate content.
 The "Estimated efforts goes here" is a placeholder for you to fill in with the estimated efforts to review the PR. It should be filled on the scale of 5 with appropriate emojis. You should use ":large_blue_circle:" for highlighting the efforts and remaining fill with ":white_circle:" emojis.
-The "Recommended focus area for review" section should be filled with the appropriate focus areas.
+The "Recommended focus area 1 goes here" section should be filled with the appropriate focus area. You can add more focus areas if needed. 
 
 Please adhere strictly to the following markdown format for the PR summary:
 #### :rocket:PR Reviewer Guide
@@ -40,10 +41,7 @@ Please adhere strictly to the following markdown format for the PR summary:
 ##### :stopwatch: Estimated efforts to review: "Estimated efforts goes here"
 
 ##### :zap: Recommended focus area for review
-- First item
-- Second item
-- Third item
-- Fourth item
+- "Recommended focus area 1 goes here"
 """
 
 PR_SUMMARY_TITLE_INTRO = """
