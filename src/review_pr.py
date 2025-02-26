a4dec2a73193c09f97507b80783e304a6e813b09
Keril Patel <keriljayeshbhai.patel@zebra.com>
2025-02-26 13:22:48 +0530

    wip: updated pr review sumary funcation

--- src/review_pr.py
+++ src/review_pr.py
@@ -15,25 +15,32 @@ class ReviewPR:
     
     def review_pr(self):
         pr_diffs = self.github_client.get_pr_diff_files()
+        pr_info = self.github_client.get_pr_info()
 
         get_pr_summary = EnvironmentVariableHelper.get_pr_summary()
         if get_pr_summary:
-            self.get_pr_summary(pr_diffs=pr_diffs)
+            self.get_pr_summary(pr_diffs=pr_diffs, pr_info=pr_info)
     
-    def get_pr_summary(self, pr_diffs: dict[str, Any]) -> None:
+    def get_pr_summary(self, pr_diffs: dict[str, Any], pr_info: dict[str, Any]) -> None:
         if not pr_diffs or "files" not in pr_diffs or not pr_diffs["files"]:
             logger.warning("No pr diff files, pr summary ignored")
             return
+        pr_title = pr_info["title"]
+        pr_description = pr_info["body"]
         pr_content_patches = [diff_item["patch"] for diff_item in pr_diffs["files"] if diff_item["filename"].find("/tests/") == -1]
         commit_messages = [commit["commit"]["message"] for commit in pr_diffs["commits"]]
+        logger.warning(pr_title)
+        logger.warning(pr_description)
         logger.warning(pr_content_patches)
         logger.warning(commit_messages)
         messages: list[dict[str, str]] = []
         format_gpt_message(messages, [PR_SUMMARY_SYSTEM_PROMPT], role=MODEL_SYSTEM_ROLE)
+        format_gpt_message(messages, [PR_SUMMARY_TITLE_INTRO + pr_title], role=MODEL_USER_ROLE)
+        format_gpt_message(messages, [PR_SUMMARY_DESCRIPTION_INTRO + pr_description], role=MODEL_USER_ROLE)
         format_gpt_message(messages, [PR_SUMMARY_COMMIT_MESSAGES_INTRO + "\n".join(commit_messages)], role=MODEL_USER_ROLE)
         format_gpt_message(messages, [PR_SUMMARY_PATCHES_INTRO + "\n".join(pr_content_patches)], role=MODEL_USER_ROLE)
         logger.warning(messages)
-        gpt_resp = self.azure_openai_client.request_gpt(messages)
-        if not gpt_resp:
-            return
-        logger.warning(gpt_resp)
+        # gpt_resp = self.azure_openai_client.request_gpt(messages)
+        # if not gpt_resp:
+        #     return
+        # logger.warning(gpt_resp)
