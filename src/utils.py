7ad9f36330b6a59d06deb6017381428465e8aad4
Keril Patel <keriljayeshbhai.patel@zebra.com>
2025-02-27 13:00:00 +0530

    wip: added logging

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+from typing import Any
 from src.constants import *
 
 
@@ -14,3 +15,37 @@ def format_gpt_message(messages: list[dict[str, str]], contents: list[str], role
             "role": role,
             "content": content
         })
+
+def generate_html_table(table_data: dict[str, Any]) -> str:
+    """
+    generate html table from table data
+    :param table_data: table data
+    :return: html table
+    """
+    table = f"""<table>"""
+    for category, suggestions in table_data.items():
+        table += f"""
+        <tr>
+            <td>{category.replace("_", " ").title()}</td>
+        """
+        if(suggestions):
+            table += f"""
+            <td>
+                <ul>
+            """
+            for suggestion in suggestions:
+                table += f"""
+                <li>{suggestion}</li>
+                """
+            table += f"""
+                </ul>
+            </td>
+            """
+        else:
+            table += f"""
+            <td>No suggestions</td>
+            """
+        
+    
+    table += f"""</table>"""
+    print(table)
