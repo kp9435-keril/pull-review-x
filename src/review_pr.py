2ebe667ee7034294c54404d1e5534c8ea3fc15ea
Keril Patel <keriljayeshbhai.patel@zebra.com>
2025-02-27 17:17:18 +0530

    feat: pr_comment_suggested_changes

--- src/review_pr.py
+++ src/review_pr.py
@@ -32,27 +32,33 @@ class ReviewPR:
         if not pr_diffs or "commits" not in pr_diffs or not pr_diffs["commits"]:
             logger.warning("No pr commits, pr summary ignored")
             return
+        
         pr_title = pr_info["title"] if ("title" in pr_info and pr_info["title"] is not None) else ""
         pr_description = pr_info["body"] if ("body" in pr_info and pr_info["body"] is not None) else ""
         pr_content_patches = [diff_item["patch"] for diff_item in pr_diffs["files"] if diff_item["filename"].find("/tests/") == -1]
         commit_messages = [commit["commit"]["message"] for commit in pr_diffs["commits"]]
+
         messages: list[dict[str, str]] = []
         format_gpt_message(messages, [PR_SUMMARY_SYSTEM_PROMPT], role=MODEL_SYSTEM_ROLE)
         format_gpt_message(messages, [PR_SUMMARY_TITLE_INTRO + pr_title], role=MODEL_USER_ROLE)
         format_gpt_message(messages, [PR_SUMMARY_DESCRIPTION_INTRO + pr_description], role=MODEL_USER_ROLE)
         format_gpt_message(messages, [PR_SUMMARY_COMMIT_MESSAGES_INTRO + "\n".join(commit_messages)], role=MODEL_USER_ROLE)
         format_gpt_message(messages, [PR_SUMMARY_PATCHES_INTRO + "\n".join(pr_content_patches)], role=MODEL_USER_ROLE)
+        
         gpt_resp = self.azure_openai_client.request_gpt(messages)
         if not gpt_resp:
             return
         self.github_client.post_pr_comment(gpt_resp)
+        
         return
     
-    def get_pr_suggest_changes(self, pr_diffs: dict[str, Any], pr_info: dict[str, Any]) -> None:
+    def get_pr_suggest_changes(self, pr_diffs: dict[str, Any]) -> None:
         if not pr_diffs or "files" not in pr_diffs or not pr_diffs["files"]:
             logger.warning("No pr diff files, pr suggest changes ignored")
             return 
+        
         pr_diff_contents = [diff_item for diff_item in pr_diffs["files"] if diff_item["filename"].find("/tests/") == -1]
+        
         messages: list[dict[str, str]] = []
         format_gpt_message(messages, [PR_SUGGEST_CHANGES_SYSTEM_PROMPT], role=MODEL_SYSTEM_ROLE)
         for diff_item in pr_diff_contents:
@@ -61,9 +67,31 @@ class ReviewPR:
             diff_patch = diff_item["patch"]
             file_content = self.github_client.get_file_contents(diff_item["contents_url"])
             format_gpt_message(messages, [FILE_CHANGES_TEMPLATE.format(diff_sha, diff_filename, diff_patch, file_content)], role=MODEL_USER_ROLE)
+        
         gpt_resp = self.azure_openai_client.request_gpt(messages)
         if not gpt_resp:
             return
         comment = generate_changes_suggestion_comment(json.loads(gpt_resp))
         self.github_client.post_pr_comment(comment)
+
+        get_pr_comment_suggested_changes = EnvironmentVariableHelper.get_pr_comment_suggested_changes()
+        if get_pr_comment_suggested_changes:
+            self.pr_comment_suggested_changes(json.loads(gpt_resp))
+
+        return
+    
+    def pr_comment_suggested_changes(self, suggested_changes: dict[str, list[dict[str, str]]]) -> None:
+        if not suggested_changes:
+            logger.warning("No suggested changes, pr comment suggested changes ignored")
+            return
+        for _ , values in suggested_changes.items():
+            for suggestion in values:
+                comment = {
+                    "body": get_comment_body(suggestion["suggestion_title"], suggestion["suggestion_description"]),
+                    "commit_id": suggestion["diff_sha"],
+                    "path": suggestion["filename"],
+                    "line": get_patch_position(suggestion["diff_patch"]),
+                    "side": "RIGHT"
+                }
+                self.github_client.post_review_comment(comment)
         return
