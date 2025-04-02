a4bab49325168856df3e76339fa8cb7b6bacf98d
Keril Patel <keriljayeshbhai.patel@zebra.com>
2025-04-02 18:37:29 +0530

    refactor: added space for better read

--- src/github_client.py
+++ src/github_client.py
@@ -12,9 +12,11 @@ class GitHubClient:
         self.token = EnvironmentVariableHelper.get_github_auth_token()
         if not self.token:
             raise GitTokenMissingException("GitHub Token is required for REST APIs")
+        
         self.repositoryLink = EnvironmentVariableHelper.get_repo()
         if not self.repositoryLink:
             raise RepoMissingException("Repository is required for REST APIs")
+        
         self.owner, self.repo = self.repositoryLink.split("/")
 
     def get_pr_info(self):
